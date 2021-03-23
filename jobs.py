6dba07ca2e64298d576147d604a821ad0acb5908
justincurl <justincurl13@gmail.com>
2021-03-22 23:18:21 -0400

    ap scheduler

--- jobs.py
+++ jobs.py
@@ -1,9 +1,9 @@
-rom apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from bot_coordination import run_all_bots
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour='23', minute='18', timezone='America/New_York')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour='23', minute='25', timezone='America/New_York')
 def run():
  	run_all_bots()
 
