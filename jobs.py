c87968d8442d7e6e034dbf42c94cf28c4f3d1370
justincurl <justincurl13@gmail.com>
2021-03-22 23:12:06 -0400

    .

--- jobs.py
+++ jobs.py
@@ -3,7 +3,7 @@ from bot_coordination import run_all_bots
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour='23', minute='10', timezone='America/New_York')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour='23', minute='18', timezone='America/New_York')
 def run():
  	run_all_bots()
 
