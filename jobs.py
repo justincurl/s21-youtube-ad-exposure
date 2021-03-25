d1b88dafc50be97afbfc11d20dc2266f72edd1f5
justincurl <justincurl13@gmail.com>
2021-03-25 18:07:00 -0400

    new time

--- jobs.py
+++ jobs.py
@@ -3,7 +3,7 @@ from bot_coordination import run_all_bots
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour='22', minute='30', timezone='America/New_York')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour='6', minute='15', timezone='America/New_York')
 def run():
 	print('starting job')
 	run_all_bots()
