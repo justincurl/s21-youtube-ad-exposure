7a8862a9487743156af3c3f47a7491f8b70bd3ef
justincurl <justincurl13@gmail.com>
2021-03-28 19:43:49 -0400

    no scheduler

--- jobs.py
+++ jobs.py
@@ -1,11 +1,4 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from bot_coordination import run_all_bots
 
-sched = BlockingScheduler()
-
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour='10', minute='5', timezone='America/New_York')
-def run():
-	print('starting job')
-	run_all_bots()
-
-sched.start()
+run_all_bots()
